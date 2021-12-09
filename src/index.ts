59e14a6688c9cf19e5f5052a4cc55f2423d912a3
Darvesh <hello@darve.sh>
2021-12-10 00:13:28 +0530

    feat: add username to message

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { Bot } from "grammy";
 import parseTime from "human-interval";
 
-const bot = new Bot("");
+import { TOKEN } from "./config";
+
+const bot = new Bot(TOKEN);
 
 const test =
   /^!remindme\s(\d{1,5}(\.\d{1,3})?\s?(s|sec|m|min|h|hr|d|day|second|seconds|minute|minutes|hour|hours|day|days)\s?)+$/;
 
 bot.hears(test, async (ctx) => {
   if (!ctx.message?.text) return;
+  if (!ctx.message.from?.username) return;
   const rawTime = ctx.message.text
     .substring(10)
     .replace(/d/g, "day")
@@ -21,9 +24,12 @@ bot.hears(test, async (ctx) => {
   });
   setTimeout(async () => {
     try {
-      await ctx.reply("Here is your reminder! ⏰", {
-        reply_to_message_id: ctx.message.message_id,
-      });
+      await ctx.reply(
+        `Here is your reminder! @${ctx.message.from?.username}⏰`,
+        {
+          reply_to_message_id: ctx.message.message_id,
+        }
+      );
     } catch (error) {
       console.log((error as Error).message);
     }

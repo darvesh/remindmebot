3259faa84e26299735233ccfa6ff1227894a6a3c
Darvesh <hello@darve.sh>
2021-12-10 00:01:18 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { Bot } from "grammy";
+import parseTime from "human-interval";
+
+const bot = new Bot("");
+
+const test =
+  /^!remindme\s(\d{1,5}(\.\d{1,3})?\s?(s|sec|m|min|h|hr|d|day|second|seconds|minute|minutes|hour|hours|day|days)\s?)+$/;
+
+bot.hears(test, async (ctx) => {
+  if (!ctx.message?.text) return;
+  const rawTime = ctx.message.text
+    .substring(10)
+    .replace(/d/g, "day")
+    .replace(/m|min/g, "minute")
+    .replace(/s|sec/g, "second")
+    .replace(/h|hr/g, "hour");
+  const time = parseTime(rawTime);
+  if (!time) return;
+  await ctx.reply(`Reminder set! 💎`, {
+    reply_to_message_id: ctx.message.message_id,
+  });
+  setTimeout(async () => {
+    try {
+      await ctx.reply("Here is your reminder! ⏰", {
+        reply_to_message_id: ctx.message.message_id,
+      });
+    } catch (error) {
+      console.log((error as Error).message);
+    }
+  }, time);
+});
+
+bot.catch((error) => console.error(error.message));
+
+bot.start();

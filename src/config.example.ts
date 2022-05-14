6dfb40215a61b1b2ffc679c6ff8a110e3529d044
Darvesh <hello@darve.sh>
2022-05-15 01:04:55 +0530

    delete

--- src/config.example.ts
+++
@@ -1,7 +0,0 @@
-/**Telegram bot token */
-export const TOKEN = "";
-
-export const SCHEDULER_TIME = 5 ;//minutes
-
-/**Master id */
-export const MY_USER_ID = 123
--- src/constant.ts
+++
@@ -1,2 +0,0 @@
-export const PATTERN =
-	/^!remindme\s(\d{1,5}(\.\d{1,3})?\s?(s|sec|m|min|h|hr|d|day|second|seconds|minute|minutes|hour|hours|day|days)\s?)+$/;

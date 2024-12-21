bab29e1024fbcf510bbe9845e551876044123202
Darvesh <hello@darve.sh>
2024-12-21 20:42:57 +0530

    chore: run deploy

--- dev.ts
+++ dev.ts
@@ -3,6 +3,6 @@ import { bot } from "./bot.ts";
 bot.catch(console.error);
 
 bot.start({
-	onStart: () => console.log("Bot started!"),
+	onStart: () => console.log("Bot started! 🚀"),
 	drop_pending_updates: true,
 });
